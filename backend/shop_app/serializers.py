2fd9814262cd5f826ec4e018edc074547067a0e0
MeKJR-sYsDeveloper <jossyyasub@gmail.com>
2025-11-07 23:04:24 +0300

    pushed latest backend code before cleanup

--- backend/shop_app/serializers.py
+++ backend/shop_app/serializers.py
@@ -27,6 +27,15 @@ class CartSerializer(serializers.ModelSerializer):
     class Meta:
         model=Cart
         fields= ["id", "cart_code", "created_at", "modified-at"]
+class SimpleCartSerializer(serializers.ModelSerializer):
+    get_num_of_items=serializers.SerializerMethodField
+    class Meta:
+        model=Cart
+        fields= ["id","cart-code", "num_of_items"]
+    def get_num_of_items(self,cart):
+        num_of_items= sum([item.quantity for item in cart.items.all()])
+        return num_of_items
+
 class CartItemSerializer(serializers.ModelSerializer):
     product = ProductSerializer(read_only=True)
     cart= CartSerializer(read_only=True)
