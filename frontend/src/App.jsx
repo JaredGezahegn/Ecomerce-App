944ae649d5734cd9d00bed6b893385e26becb9f6
JaredGezahegn <jaredgezahegn@gmail.com>
2025-11-27 16:40:26 +0300

    productPage and related products

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import MainLayout from './layout/MainLayout';
 import Homepage from './components/Home/Homepage';
 import NotFoundPage from './components/ui/NotFoundPage';
+import ProductPage from './components/product/ProductPage';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
        <Routes>
           <Route path="/" element={<MainLayout/>}>
           <Route index element={<Homepage/>}/>
+          <Route path="/products/:slug" element={<ProductPage/>}/>
           </Route>
           <Route path="*" element={<NotFoundPage />} />
           
