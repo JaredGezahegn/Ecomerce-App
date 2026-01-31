8be768dbf7ca45fd86a058bf934c049ee4c6ccfb
MeKJR-sYsDeveloper <jossyyasub@gmail.com>
2026-01-31 17:07:03 +0300

    payment initiatation

--- backend/shop_app/views.py
+++ backend/shop_app/views.py
@@ -3,8 +3,9 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
+from django.conf import settings
 
-from .models import Product, Cart, CartItem
+from .models import Product, Cart, CartItem, Transaction
 from .serializers import (
     CartSerializer,
     ProductSerializer,
@@ -14,7 +15,11 @@ from .serializers import (
     UserSerializer
 )
 
+from decimal import Decimal
+import uuid
+import requests
 
+BASE_URL = "http://localhost:5173"
 
 def home(request):
   return render(request, 'home.html')
@@ -178,178 +183,67 @@ def get_username(request):
 @api_view(["GET"])
 @permission_classes([IsAuthenticated])
 def user_info(request):
+    user= request.user
     serializer = UserSerializer(request.user)
     return Response(serializer.data)
 
-
-
-
-'''from django.shortcuts import render
-#from django.http import HttpResponse
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated
-from .models import Product,Cart,CartItem
-from .serializers import CartSerializer,ProductSerializer,DetailedProductSerializer, CartItemSerializer, SimpleCartSerializer, UserSerializer
-from rest_framework.response import Response
-from rest_framework import status
-from django.shortcuts import get_object_or_404
-
-
-# Create your views here.
-def home(request):
-    return render(request, 'home.html')
-
-#def home(request):
-    #return HttpResponse("Welcome to Shoppit!")
-
-@api_view(["GET"])
-def products(request):
-    products= Product.objects.all()
-    serializers= ProductSerializer(products, many=True)
-    return Response(serializers.data)
-@api_view(["GET"])
-def product_detail(request, slug):
-    product = get_object_or_404(
-        Product.objects.select_related("dimensions", "meta").prefetch_related("reviews"),
-        slug=slug
-    )
-
-   
-    similar_products = Product.objects.filter(
-        category=product.category
-    ).exclude(id=product.id)[:10]
- 
-
-    
-    serializer = DetailedProductSerializer(
-        product,
-        context={
-            "similar_products": similar_products,
-            "request": request
-        }
-    )
-
-    return Response(serializer.data, status=status.HTTP_200_OK)
-
- 
 @api_view(["POST"])
-def add_item(request):
-    cart_code = request.data.get("cart_code")
-    product_id = request.data.get("product_id")
-    quantity = request.data.get("quantity", 1)  
-
-    
-    if not cart_code or not product_id:
-        return Response(
-            {"error": "cart_code and product_id are required"},
-            status=400
-        )
-
- 
-    try:
-        quantity = int(quantity)
-        if quantity < 1:
-            return Response({"error": "quantity must be at least 1"}, status=400)
-    except:
-        return Response({"error": "quantity must be a number"}, status=400)
-
-    
-    cart, _ = Cart.objects.get_or_create(cart_code=cart_code)
-    product = get_object_or_404(Product, id=product_id)
-
-  
-    cartitem, created = CartItem.objects.get_or_create(cart=cart, product=product)
-
-    if not created:
-        cartitem.quantity += quantity
-    else:
-        cartitem.quantity = quantity
-
-    cartitem.save()
-
-    serializer = CartItemSerializer(cartitem)
-    return Response(
-        {
-            "message": "Item added to cart successfully",
-            "cart_item": serializer.data
-        },
-        status=201
-    )
-
-   
-   
-   
-@api_view(['GET'])
-def product_in_cart(request):
-    cart_code = request.query_params.get("cart_code")
-    product_id = request.query_params.get("product_id")
-
-    if not cart_code or not product_id:
-        return Response(
-            {"error": "cart_code and product_id are required"},
-            status=400
-        )
-
-    cart = get_object_or_404(Cart, cart_code=cart_code)
-    product = get_object_or_404(Product, id=product_id)
-
-    product_exists_in_cart = CartItem.objects.filter(
-        cart=cart,
-        product=product
-    ).exists()
-
-    return Response({"product_in_cart": product_exists_in_cart})
-@api_view(['GET'])
-def get_cart_stat(request):
-     cart_code=request.query_params.get("cart_code")
-     cart=Cart.objects.get(cart_code=cart_code, paid=False)
-     serializer= SimpleCartSerializer(cart)
-     return Response(serializer.data)
-@api_view(['GET'])
-def get_cart(request):
-     cart_code=request.query_params.get("cart_code")
-     cart=Cart.objects.get(cart_code=cart_code, paid=False)
-     serializer=CartSerializer(cart)
-     return Response(serializer.data)
-@api_view(['PATCH'])
-def update_quantity(request):
-    try:
-          cartitem_id= request.data.get("item_id")
-          quantity = request.data.get("quantity")
-          quantity=int(quantity)
-          cartitem= cartitem.objects.get(id=cartitem_id)
-          cartitem.quantity=quantity
-          cartitem.save()
-          serializer = CartItemSerializer(cartitem)
-          return Response({"data":serializer.data, "message": "Cartitem updated successfully!"})
-    except Exception as e:
-         return Response({'error':str(e)}, status=400)
-
-@api_view(['POST'])
-def import_products(request):
-    from .serializers import DetailedProductSerializer
-
-    products = request.data.get('products', [])
-    results = []
-
-    for item in products:
-        serializer = DetailedProductSerializer(data=item)
-        if serializer.is_valid():
-            serializer.save()
-            results.append({'name': item['title'], 'status': 'imported'})
-        else:
-            results.append({'name': item.get('title', 'Unknown'), 'errors': serializer.errors})
-
-    return Response(results)
-@api_view(["GET"])
 @permission_classes([IsAuthenticated])
-def get_username(request):
-     user= request.user
-     return Response({"username": user.username})
+def initiate_payment(request):
+    if request.user:
+        try:
+            tx_ref = str(uuid.uuid4())
+            cart_code = request.data.get("cart_code")
+            cart = Cart.objects.get(cart_code=cart_code)
+            user = request.user
 
-@api_view(["GET"])
-@permission_classes([IsAuthenticated])
-def user_info(request):
-     user=request.user
-     serializer= UserSerializer(user)
-     return Response(serializer.data)'''
+            # Ensure we sum with Decimal from the start to avoid mixing numeric types
+            amount = sum((item.quantity * item.product.price for item in cart.items.all()), Decimal('0.00'))
+            tax = Decimal("15.00")
+            total_amount = amount + tax
+            currency = "ETB"
+            redirect_url = f"{BASE_URL}/payment_status/"
+
+            transaction = Transaction.objects.create(
+                ref=tx_ref,
+                cart=cart,
+                amount=total_amount,
+                currency=currency,
+                user=user,
+                status='pending'
+            )
+            flutterwave_payload = {
+                "tx_ref": tx_ref,
+                "amount": str(total_amount),
+                "currency": currency,
+                "redirect_url": redirect_url,
+                "customer": {
+                    "email": user.email,
+                    "name": user.username,
+                    "phonenumber": user.phone
+
+                },
+                "customizations": {
+                    "title": "Shoppit Payment"
+                },
+            }
+
+            headers = {
+                "Authorization": f"Bearer {settings.FLUTTERWAVE_SECRET_KEY}",
+                "Content-Type": "application/json"
+            }
+            response = requests.post(
+                "https://api.flutterwave.com/v3/payments",
+                json=flutterwave_payload,
+                headers=headers
+            )
+            if response.status_code ==200:
+                return Response(response.json(), status=status.HTTP_200_OK)
+            else:
+                return Response(response.json(), status=response.status_code)
+        except requests.exceptions.RequestException as e:
+            return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+
+
